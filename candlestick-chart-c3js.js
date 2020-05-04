2d5c32373e2753567261bec2f1c97fd503c20d73
Sergio1192 <sergio11_92@hotmail.com>
2020-05-04 17:44:35 +0100

    Cambiamos de lugar el tag script

--- candlestick-chart-c3js.js
+++ candlestick-chart-c3js.js
@@ -2,61 +2,61 @@
 
 let realData = [];
 for(let i = new Date('2018/01/01'); i < new Date('2018/02/01'); i.setDate(i.getDate() + 1)) {
-  let date = `${i.getFullYear()}-${i.getMonth()+1}-${i.getDate()}T00:00:00`;
-	let data = { 
-  	x: date, low:49.1, open:50, close:55, high:55.9, serieOne:61, serieTwo:59, serieThree: 58 
-  };
-	realData.push(data);
+    let date = `${i.getFullYear()}-${i.getMonth()+1}-${i.getDate()}T00:00:00`;
+    let data = { 
+        x: date, low:49.1, open:50, close:55, high:55.9, serieOne:61, serieTwo:59, serieThree: 58 
+    };
+    realData.push(data);
 }
 
 var chart = {
-  bindto: '#chart',
-  data: {
+    bindto: '#chart',
+    data: {
     json: realData,
     xFormat: '%Y-%m-%dT%H:%M:%S',
     keys: {
-      x: "x",
-      value: [
+        x: "x",
+        value: [
         "serieOne",
         "serieTwo",
         "serieThree"
-      ]
+        ]
     },
     type: 'line',
     colors: {
-      serieOne: "green",
-      serieTwo: "yellow",
-      serieThree: "red"
+        serieOne: "green",
+        serieTwo: "yellow",
+        serieThree: "red"
     },
     names: {
-      serieOne: '1',
-      serieTwo: '2',
-      serieThree: '3'
+        serieOne: '1',
+        serieTwo: '2',
+        serieThree: '3'
     }
-  },
-  axis: {
+    },
+    axis: {
     x: {
-      type: 'timeseries',
-      tick: {
+        type: 'timeseries',
+        tick: {
         format: "%d-%m-%Y"
-      }
+        }
     }
-  },
-  zoom: {
+    },
+    zoom: {
     enabled: true,
     rescale: true
-  },
-  subchart: {
+    },
+    subchart: {
     show: true,
     size: {
-      height: 10
+        height: 10
     }
-  },
-  grid: {
+    },
+    grid: {
     y: {
-      show: true
+        show: true
     }
-  }
+    }
 };
                     
 candlestickChart(chart, "blue", "red", "Candlestick");
@@ -64,107 +64,107 @@ candlestickChart(chart, "blue", "red", "Candlestick");
 
 // Main Candlestick function
 function candlestickChart (chart, colorUp, colorDown, candlestickName) {
-	let realData = chart.data.json;
-  let selector = chart.bindto;
-  let xAxisName = chart.data.keys.x;	
-  let typeXAxis = chart.axis && chart.axis.x && chart.axis.x.type;
-  
-	let stackedData = toStackedData(realData, xAxisName, typeXAxis);
-	let data = stackedData.data, dic = stackedData.dic, min = stackedData.min;
-  
-  $("#candlestickUp").attr("stop-color", colorUp);
-  $("#candlestickDown").attr("stop-color", colorDown);
-  
-  $(selector).data("dic", dic);
-  $(selector).data("colorUp", colorUp);
-  $(selector).data("colorDown", colorDown);
-  
-  let chartWithCandlestick = chartToChartWithCandlestick(chart, data, dic, colorUp, colorDown, candlestickName, min);
+    let realData = chart.data.json;
+    let selector = chart.bindto;
+    let xAxisName = chart.data.keys.x;	
+    let typeXAxis = chart.axis && chart.axis.x && chart.axis.x.type;
+    
+    let stackedData = toStackedData(realData, xAxisName, typeXAxis);
+    let data = stackedData.data, dic = stackedData.dic, min = stackedData.min;
+    
+    $("#candlestickUp").attr("stop-color", colorUp);
+    $("#candlestickDown").attr("stop-color", colorDown);
+    
+    $(selector).data("dic", dic);
+    $(selector).data("colorUp", colorUp);
+    $(selector).data("colorDown", colorDown);
+    
+    let chartWithCandlestick = chartToChartWithCandlestick(chart, data, dic, colorUp, colorDown, candlestickName, min);
 
-  c3.generate(chartWithCandlestick);
-  
-  bugSvgGradientColorInLine(selector);
+    c3.generate(chartWithCandlestick);
+    
+    bugSvgGradientColorInLine(selector);
 }
 
 // "private" functions
 function bugSvgGradientColorInLine (selector) {
-  let selectorLine = $(selector + " g.c3-legend-item-low line.c3-legend-item-tile");
-  let y1 = selectorLine.attr("y1");
-  let y2 = selectorLine.attr("y2");
-  
-  if (y1 === y2) {
-  	selectorLine.attr("y2", parseInt(y2) + 0.01);
-  }
+    let selectorLine = $(selector + " g.c3-legend-item-low line.c3-legend-item-tile");
+    let y1 = selectorLine.attr("y1");
+    let y2 = selectorLine.attr("y2");
+    
+    if (y1 === y2) {
+        selectorLine.attr("y2", parseInt(y2) + 0.01);
+    }
 }
 
 function chartToChartWithCandlestick (chart, data, dic, colorUp, colorDown, candlestickName, min) {
-  var candlestickChart = {
+    var candlestickChart = {
     data: {
-    	json: data,
-      names: {
+        json: data,
+        names: {
         low: candlestickName
-      },
-      order: null,
-      colors: {
+        },
+        order: null,
+        colors: {
         low: "url(#gradientCandlestickChartC3)",
         open: colorUp,
         close: colorUp,
         high: colorUp
-      },
-      color: function (color, d) { 
-      	return candlestickColorFunction(color, colorDown, d, dic); 
-      },
-      types: {
+        },
+        color: function (color, d) { 
+            return candlestickColorFunction(color, colorDown, d, dic); 
+        },
+        types: {
         low: 'bar',
         open: 'bar',
         close: 'bar',
         high: 'bar'
-      },
+        },
     },
     axis: {
-      x: {
+        x: {
         tick: {
-          count: null
+            count: null
         }
-      },
-      y: {
+        },
+        y: {
         min: min
-      }
+        }
     },
     legend: {
-      item: {
+        item: {
         onclick: onClickItemLegendFunction,
         onmouseover: onMouseOverLegendFunction,
-      }
+        }
     },
     tooltip: {
-      format: {
+        format: {
         name: nameFormatTooltip
-      },
-     	contents: contentsTooltipFunction
+        },
+        contents: contentsTooltipFunction
     }
-  };
-  
-  chart = $.extend(true, {}, chart, candlestickChart);
-  
-  chart.data.keys.value = chart.data.keys.value == null ? [] : chart.data.keys.value;
-	chart.data.keys.value = chart.data.keys.value.concat(["low", "open", "close", "high"]);
-  
-  chart.data.groups = chart.data.groups == null ? [] : chart.data.groups;
-  chart.data.groups.push(["low", "open", "close", "high"]);
-  
-  chart.legend.hide = chart.legend.hide == null ? [] : chart.legend.hide;
-  chart.legend.hide = chart.legend.hide.concat(["open", "close", "high"]); 
-  
-  return chart;
+    };
+    
+    chart = $.extend(true, {}, chart, candlestickChart);
+    
+    chart.data.keys.value = chart.data.keys.value == null ? [] : chart.data.keys.value;
+    chart.data.keys.value = chart.data.keys.value.concat(["low", "open", "close", "high"]);
+    
+    chart.data.groups = chart.data.groups == null ? [] : chart.data.groups;
+    chart.data.groups.push(["low", "open", "close", "high"]);
+    
+    chart.legend.hide = chart.legend.hide == null ? [] : chart.legend.hide;
+    chart.legend.hide = chart.legend.hide.concat(["open", "close", "high"]); 
+    
+    return chart;
 }
 
 function toStackedData (realData, xAxisName, typeXAxis) {
-	let data = [], dic = {}, min = Number.MAX_SAFE_INTEGER;
-  realData.forEach(function (element) {
-  	let x = element[xAxisName];
+    let data = [], dic = {}, min = Number.MAX_SAFE_INTEGER;
+    realData.forEach(function (element) {
+        let x = element[xAxisName];
     if(typeXAxis === "timeseries")
-    	x = new Date(element[xAxisName].replace("T", " "));
+        x = new Date(element[xAxisName].replace("T", " "));
     dic[x] = element;
 
     let low = element.low;    
@@ -173,7 +173,7 @@ function toStackedData (realData, xAxisName, typeXAxis) {
     let open = element.open <= element.close ? oc : co;
     let close = (element.open <= element.close ? element.close : element.open) - (low + open);
     let high = element.high - (low + open + close);
-		
+        
     let e = $.extend({}, element);
     e.low = low;
     e.open = open;
@@ -183,86 +183,86 @@ function toStackedData (realData, xAxisName, typeXAxis) {
     data.push(e);
     
     min = getMinFromObject(element, min, xAxisName);
-  });
-  
-  return { data, dic, min };
+    });
+    
+    return { data, dic, min };
 }
 
 function getMinFromObject(element, min, xAxisName) {
-  let min2 = min;
-  for (var nameProperty in element) {
+    let min2 = min;
+    for (var nameProperty in element) {
     if (nameProperty !== xAxisName) {
-      let v = element[nameProperty];
-      min2 = (v < min2) ? v : min2;
+        let v = element[nameProperty];
+        min2 = (v < min2) ? v : min2;
     }
-  }
+    }
 
-  return min2;
+    return min2;
 }
 
 function candlestickColorFunction (colorUp, colorDown, d, dic) {
-	if (d.id === "low") {
+    if (d.id === "low") {
     return "transparent";
-  }
-  else if (d.id === "close" && dic[d.x] != null) {
+    }
+    else if (d.id === "close" && dic[d.x] != null) {
     return dic[d.x]["open"] <= dic[d.x][d.id] ? colorUp : colorDown;
-  }
+    }
 
-  return colorUp;
+    return colorUp;
 }
 
 // Events
 var refreshIntervalId;
 function onClickItemLegendFunction (id) {
-	var chart = $(this)[0];
-	if(id === "low") {
+    var chart = $(this)[0];
+    if(id === "low") {
     chart.api.toggle(["low", "open", "high", "close"]);
-  } else {
-  	chart.api.toggle(id);
-  }
-  
-  refreshIntervalId = setInterval(function () {
+    } else {
+        chart.api.toggle(id);
+    }
+    
+    refreshIntervalId = setInterval(function () {
     bugSvgGradientColorInLine(chart.config.bindto);
     clearInterval(refreshIntervalId);
-  }, 250);
+    }, 250);
 }
 
 function onMouseOverLegendFunction (id) {
-  let chart = $(this)[0];
-  if(id === "low") {
+    let chart = $(this)[0];
+    if(id === "low") {
     chart.api.focus(["low", "open", "high", "close"]);
-  } else {
+    } else {
     chart.api.focus(id);
-  }
+    }
 }
 
 function nameFormatTooltip (name, ratio, id, index) { 
-  return id === "low" ? id : name; 
+    return id === "low" ? id : name; 
 }
         
 function contentsTooltipFunction (d, defaultTitleFormat, defaultValueFormat, color) {
-  let chart = $(this)[0];
-  let selector = chart.config.bindto;
-  let dic = $(selector).data("dic");
-  let colorUp = $(selector).data("colorUp");
-  let colorDown = $(selector).data("colorDown");
-  
-  let defaultValueFormat2 = function (v, ratio, id) {
-  	let v2 = v;
+    let chart = $(this)[0];
+    let selector = chart.config.bindto;
+    let dic = $(selector).data("dic");
+    let colorUp = $(selector).data("colorUp");
+    let colorDown = $(selector).data("colorDown");
+    
+    let defaultValueFormat2 = function (v, ratio, id) {
+        let v2 = v;
     if((id === "low" || id === "open" || id === "close" || id === "high") && dic[d[0].x] != null) {
-      v2 = dic[d[0].x][id];
+        v2 = dic[d[0].x][id];
     }
     
     return defaultValueFormat(v2, ratio, id)
-  }
-  
-  let color2 = function (id) {
-    if((id === "low" || id === "open" || id === "close" || id === "high") && dic[d[0].x] != null) {
-      return dic[d[0].x]["open"] <= dic[d[0].x]["close"] ? colorUp : colorDown;
-    } else {
-      return color(id);
     }
-  };
+    
+    let color2 = function (id) {
+    if((id === "low" || id === "open" || id === "close" || id === "high") && dic[d[0].x] != null) {
+        return dic[d[0].x]["open"] <= dic[d[0].x]["close"] ? colorUp : colorDown;
+    } else {
+        return color(id);
+    }
+    };
 
-  return this.getTooltipContent(d, defaultTitleFormat, defaultValueFormat2, color2);
+    return this.getTooltipContent(d, defaultTitleFormat, defaultValueFormat2, color2);
 }
